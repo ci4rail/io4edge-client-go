bc3172bc41c925ad75117c9a00e912972620b546
KlausPopp <klauspopp@gmx.de>
2021-09-08 21:22:59 +0200

    add package docs

--- pkg/io4edge/client.go
+++ pkg/io4edge/client.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package io4edge provides the API for io4edge I/O devices
 package io4edge
 
 import (
