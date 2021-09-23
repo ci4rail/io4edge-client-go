0d525143931d66cf70c65966f1f5193a6ca1370f
FranziskaKnoedtel <Franziska.Knoedtel@gmail.com>
2021-09-23 15:40:17 +0200

    add mdns browser to search for mdns services

---
+++ core/mdns_browser.go
@@ -0,0 +1,55 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/godbus/dbus/v5"
+	"github.com/holoplot/go-avahi"
+)
+
+func searchService(sb *avahi.ServiceBrowser, srv *avahi.Server, name string, t int) (avahi.Service, error) {
+	var s avahi.Service
+	for {
+		select {
+		case s = <-sb.AddChannel:
+			fmt.Println(s)
+			s, err := srv.ResolveService(s.Interface, s.Protocol, s.Name,
+				s.Type, s.Domain, avahi.ProtoUnspec, 0)
+			if err != nil {
+				log.Fatalf("ResolveService() failed: %v", err)
+			}
+			fmt.Println(s)
+			if s.Name == name {
+				return s, err
+			}
+		case <-time.After(time.Duration(t) * time.Second):
+			fmt.Println("timeout")
+			err := errors.New("could not find instance or service")
+			return s, err
+		}
+	}
+}
+
+func GetAddressFromService(instanceName string, serviceName string, timeout int) (string, uint16, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		log.Fatalf("Cannot get system bus: %v", err)
+	}
+
+	server, err := avahi.ServerNew(conn)
+	if err != nil {
+		log.Fatalf("Avahi new failed: %v", err)
+	}
+
+	sb, err := server.ServiceBrowserNew(avahi.InterfaceUnspec, avahi.ProtoUnspec, serviceName, "local", 0)
+	if err != nil {
+		log.Fatalf("ServiceBrowserNew() failed: %v", err)
+	}
+
+	service, err := searchService(sb, server, instanceName, timeout)
+
+	return service.Address, service.Port, err
+}
