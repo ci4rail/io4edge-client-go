f247242f7360b5ec46407b2c57fb811f5ff86d48
Armin Schlegel <armin.schlegel@gmx.de>
2021-12-22 12:59:40 +0100

    fixed setting config

--- binaryIoTypeA/configuration.go
+++ binaryIoTypeA/configuration.go
@@ -62,6 +62,58 @@ func (c *Client) SetConfiguration(config Configuration) error {
 	return nil
 }
 
+func (c *Client) GetConfiguration() (*Configuration, error) {
+	cmd := binio.ConfigurationControlGet{}
+	envelopeCmd, err := functionblock.ConfigurationControlSet(&cmd)
+	if err != nil {
+		return nil, err
+	}
+	res := &functionblockV1.Response{}
+	err = c.funcClient.Command(envelopeCmd, res, time.Second*5)
+	if err != nil {
+		return nil, err
+	}
+	if res.Status == functionblockV1.Status_NOT_IMPLEMENTED {
+		return nil, fmt.Errorf("not implemented")
+	}
+	if res.Status == functionblockV1.Status_ERROR {
+		return nil, fmt.Errorf(res.Error.Error)
+	}
+	get := binio.ConfigurationControlResponse{}
+	err = anypb.UnmarshalTo(res.GetConfigurationControl().FunctionSpecificConfigurationControlResponse, &get, proto.UnmarshalOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	ret := &Configuration{
+		OutputFritting: func(mask int32) map[int]bool {
+			ret := make(map[int]bool)
+			for ch := 0; ch < 4; ch++ {
+				if mask&(1<<ch) != 0 {
+					ret[ch] = true
+				}
+			}
+			return ret
+		}(int32(get.GetGet().OutputFrittingMask)),
+		OutputWatchdog: func(mask int32) map[int]bool {
+			ret := make(map[int]bool)
+			for ch := 0; ch < 4; ch++ {
+				if mask&(1<<ch) != 0 {
+					ret[ch] = true
+				}
+			}
+			return ret
+		}(int32(get.GetGet().OutputWatchdogMask)),
+		OutputWatchdogTimeout: func(timeout int32) *int {
+			if timeout == -1 {
+				return nil
+			}
+			return &timeout
+		}(int32(get.GetGet().OutputWatchdogTimeout)),
+	}
+	return nil
+}
+
 func (c *Client) Describe() (*binio.ConfigurationControlDescribeResponse, error) {
 	cmd := binio.ConfigurationControlDescribe{}
 	envelopeCmd, err := functionblock.ConfigurationControlDescribe(&cmd)
