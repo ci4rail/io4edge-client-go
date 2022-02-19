a8b7034f63b789b7ff3d9ddc890b5ebb0ce3709f
KlausPopp <klauspopp@gmx.de>
2022-02-19 21:55:45 +0100

    WIP

--- examples/mvbSniffer/generator/main.go
+++ examples/mvbSniffer/generator/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -9,15 +10,30 @@ import (
 	"github.com/ci4rail/io4edge-client-go/mvbsniffer"
 )
 
+func generatePattern() string {
+	cl := mvbsniffer.NewCommandList()
+
+	// req 32 bits process data from address 123
+	cl.AddMasterFrame(0, false, 5, 0x1, 567)
+	cl.AddMasterFrame(0, false, 3, 0x1, 888)
+	cl.AddMasterFrame(0, false, 5, 0x1, 123)
+
+	cl.AddSlaveFrame(0, false, 100, []uint8{0xaa, 0xbb, 0xcc, 0xdd})
+
+	return cl.StartGeneratorString(true)
+}
+
 func main() {
 	const timeout = 5 * time.Second
 
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: %s <mdns-service-address OR <ip:port>> <pattern>", os.Args[0])
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s <mdns-service-address OR <ip:port>>", os.Args[0])
 	}
 	address := os.Args[1]
-	pattern := os.Args[2]
 
+	pattern := generatePattern()
+
+	fmt.Printf("Generator pattern: %s\n", pattern)
 	c, err := mvbsniffer.NewClientFromUniversalAddress(address, timeout)
 	if err != nil {
 		log.Fatalf("Failed to create mvbSniffer client: %v\n", err)
