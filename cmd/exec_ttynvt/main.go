46720bc8a8d463bcaf48d1b06b5644f9ca6a4e67
FranziskaKnoedtel <Franziska.Knoedtel@gmail.com>
2022-03-21 16:14:57 +0100

    Handle drivers major and devices minor numbers

--- cmd/exec_ttynvt/main.go
+++ cmd/exec_ttynvt/main.go
@@ -17,37 +17,98 @@ limitations under the License.
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/ci4rail/io4edge-client-go/client"
 )
 
-var ttynvtInstanceMap = make(map[string]*exec.Cmd)
-var major = 199
+const maxMinorNumbers = 256
+
+type ttynvtInstanceInfo struct {
+	cmd   *exec.Cmd
+	minor int
+}
+
+var ttynvtInstanceMap = make(map[string]ttynvtInstanceInfo)
+var major int
+var minorMap [maxMinorNumbers]bool
+
+func initMinorMap() {
+	for i := range minorMap {
+		minorMap[i] = true
+	}
+}
+
+func getFreeMinor() (minor int, err error) {
+	for minor, isFree := range minorMap {
+		if isFree {
+			return minor + 1, nil
+		}
+	}
+	err = errors.New("no free minor numbers are left")
+	return 0, err
+}
+
+func setMinorOccupied(minor int) {
+	minorMap[minor-1] = false
+}
+
+func setMinorFree(minor int) {
+	minorMap[minor-1] = true
+}
 
 func serviceAdded(s client.ServiceInfo) error {
+	var err error
+	var instanceInfo ttynvtInstanceInfo
 	name := s.GetInstanceName()
 	fmt.Println("Added service ", name)
 	ipPort := s.GetIPAddressPort()
-	cmd := exec.Command("./ttynvt", "-D", "7", "-d", "-M", strconv.Itoa(major), "-m", "6", "-n", name, "-S", ipPort)
-	err := cmd.Start()
+	instanceInfo.minor, err = getFreeMinor()
 	if err != nil {
-		return err
+		log.Errorf("Start ttynvt instance (%s) failed: %v\n", name, err)
+		// return nil, that all other ttynvt instances are not terminated
+		return nil
 	}
-	ttynvtInstanceMap[name] = cmd
-	major++
+	instanceInfo.cmd = exec.Command("./ttynvt", "-D", "7", "-d", "-M", strconv.Itoa(major), "-m", strconv.Itoa(instanceInfo.minor), "-n", name, "-S", ipPort)
+	err = instanceInfo.cmd.Start()
+	if err != nil {
+		log.Errorf("Start ttynvt instance %d (%s) failed: %v\n", instanceInfo.minor, name, err)
+		// return nil, that all other ttynvt instances are not terminated
+		return nil
+	}
+	setMinorOccupied(instanceInfo.minor)
+	ttynvtInstanceMap[name] = instanceInfo
 	return nil
 }
 
 func serviceRemoved(s client.ServiceInfo) error {
 	name := s.GetInstanceName()
 	fmt.Println("Removed service ", name)
-	ttynvtInstanceMap[name].Process.Kill()
+	err := ttynvtInstanceMap[name].cmd.Process.Kill()
+	if err != nil {
+		log.Errorf("Kill ttynvt instance %d (%s) failed: %v\n", ttynvtInstanceMap[name].minor, name, err)
+		// return nil, that all other ttynvt instances are not terminated (maybe current instance wasn't ever started)
+		return nil
+	}
+	setMinorFree(ttynvtInstanceMap[name].minor)
 	return nil
 }
 
 func main() {
+	var err error
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s <driver-major-number>", os.Args[0])
+	}
+	major, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Usage: %s <driver-major-number>", os.Args[0])
+	}
+	initMinorMap()
 	client.ServiceObserver("_ttynvt._tcp", serviceAdded, serviceRemoved)
 }
