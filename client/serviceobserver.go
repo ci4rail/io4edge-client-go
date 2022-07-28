84ac8397a8c3c4042a4770368188c7188d65fa3d
KlausPopp <klauspopp@gmx.de>
2022-07-28 14:50:21 +0200

    serviceobserver: don't block service browser by waiting for resolver responses

--- client/serviceobserver.go
+++ client/serviceobserver.go
@@ -10,6 +10,9 @@ import (
 
 func runServiceBrowser(domain string, serviceType string, addServiceChan chan ServiceInfo, removeServiceChan chan ServiceInfo) {
 	log.Debugf("starting service Browser for %s", serviceType)
+	if serviceType == "" {
+		return
+	}
 	sb, err := server.ServiceBrowserNew(avahi.InterfaceUnspec, avahi.ProtoUnspec, serviceType, domain, 0)
 	if err != nil {
 		log.Errorf("servicescan: can't start service browser for %s: %v", serviceType, err)
@@ -19,15 +22,17 @@ func runServiceBrowser(domain string, serviceType string, addServiceChan chan Se
 		var svcInf ServiceInfo
 		select {
 		case s := <-sb.AddChannel:
-			log.Debugf("browser got add service %s", s.Name)
-			s, err = server.ResolveService(s.Interface, s.Protocol, s.Name,
-				s.Type, s.Domain, avahi.ProtoUnspec, 0)
-			if err == nil {
-				svcInf.service = s
-				addServiceChan <- svcInf
-			} else {
-				log.Debugf("servicescan: can't resolve service %s: %v", serviceType, err)
-			}
+			log.Debugf("browser got add service %s if=%v prot=%v type=%v domain=%v", s.Name, s.Interface, s.Protocol, s.Type, s.Domain)
+			go func() {
+				s, err = server.ResolveService(s.Interface, s.Protocol, s.Name,
+					s.Type, s.Domain, avahi.ProtoUnspec, 0)
+				if err == nil {
+					svcInf.service = s
+					addServiceChan <- svcInf
+				} else {
+					log.Debugf("servicescan: can't resolve service %s: %v", s.Name, err)
+				}
+			}()
 		case s := <-sb.RemoveChannel:
 			log.Debugf("browser got rem service %s", s.Name)
 			svcInf.service = s
