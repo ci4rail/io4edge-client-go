259ba1d4af024fe10a1edaf10f8256262bb05721
popples <klauspopp@gmx.de>
2025-11-26 10:40:38 +0100

    load firmware package: relax hardware name check to allow hwinventory to contain full part number

--- pkg/core/firmware_test.go
+++ pkg/core/firmware_test.go
@@ -10,4 +10,6 @@ func TestAssertFirmwareIsCompatibleWithHardware(t *testing.T) {
 	assert.Nil(t, AssertFirmwareIsCompatibleWithHardware("S101-IOU01", []int{1, 2, 3}, "s101-IOU01", 1))
 	assert.NotNil(t, AssertFirmwareIsCompatibleWithHardware("S101-IOU01", []int{1, 2, 3}, "s101-IOU04", 1))
 	assert.NotNil(t, AssertFirmwareIsCompatibleWithHardware("S101-IOU01", []int{1, 2, 3}, "s101-IOU01", 4))
+	assert.Nil(t, AssertFirmwareIsCompatibleWithHardware("s103-sio06", []int{1, 2}, "S103-SIO06-02-00001", 2))
+	assert.NotNil(t, AssertFirmwareIsCompatibleWithHardware("s103-sio06-02-0001", []int{1, 2}, "S103-SIO06-02", 2))
 }
